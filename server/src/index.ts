8519dc45fcf877326ae17850c774754ee530168b
Destiny Aigbe <aigbedestinyic@gmail.com>
2024-07-15 13:08:14 +0100

    fix: vercel issue

--- server/src/index.ts
+++ server/src/index.ts
@@ -16,7 +16,11 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(
+  cors({
+    origin: [process.env.CLIENT_URL || 'http://localhost:3000'],
+  })
+);
 config();
 connectDB();
 

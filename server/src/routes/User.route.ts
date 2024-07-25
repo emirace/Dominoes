4514e87ba9097ffd4126647983251ea67a395d30
Destiny Aigbe <aigbedestinyic@gmail.com>
2024-07-25 01:05:12 +0100

    fix: email error in prod

--- server/src/routes/User.route.ts
+++ server/src/routes/User.route.ts
@@ -4,8 +4,8 @@ import authMiddleware from '../middlewares/authMiddleware';
 
 const router = Router();
 
-router.post('/register', UserController.register);
-router.post('/login', UserController.login);
+// router.post('/register', UserController.register);
+// router.post('/login', UserController.login);
 router.post('/auth', UserController.authenticate);
 router.get('/me', authMiddleware, UserController.getLoggedInUser);
 router.get('/:id', UserController.getUser);
